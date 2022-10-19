c9e77fa06b7bdd85b9ff9d4cb8a7739e40188774
unknown <najmussakib173@gmail.com>
2022-10-19 11:29:16 +0600

    fire base authentication

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbdbz3ID1QjHR2pcnE0qiPCjUQ8AyFaRQ",
+  authDomain: "simple-firebase-authenti-fa5f6.firebaseapp.com",
+  projectId: "simple-firebase-authenti-fa5f6",
+  storageBucket: "simple-firebase-authenti-fa5f6.appspot.com",
+  messagingSenderId: "65381234132",
+  appId: "1:65381234132:web:f9e119d8ff263a305f1241",
+  measurementId: "G-BLX918E6EP"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
