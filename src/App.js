90deaf78b84b6dcd9fb80961bdcb5974bf480b09
unknown <najmussakib173@gmail.com>
2022-10-20 11:50:46 +0600

    load data with google email

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 const auth=getAuth(app)
-
 function App() {
+  const [user,setUser]=useState({})
   const provider = new  GoogleAuthProvider();
-
   const HandleGoogleSignin= ()=>{
     // console.log('Google comming');
     signInWithPopup(auth, provider)
     .then(result =>{
       const user=result.user;
+      setUser(user)
       console.log(user);
-
     })
     .catch(error=>{
       console.log.error('error',error);
@@ -21,8 +21,16 @@ function App() {
   return (
     <div className="App">
       <button onClick={HandleGoogleSignin} >Google Sign in</button>
+      <div>
+        user Name: {user.displayName}
+      </div>
+
+      <div>
+        Email Address: {user.email}
+      </div>
+      <img src={user.photoURL} alt=""/>
+      
     </div>
   );
 }
-
 export default App;
