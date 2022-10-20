d71679b3b70f9877677865d2c8c4b83f88d253a4
unknown <najmussakib173@gmail.com>
2022-10-20 12:25:14 +0600

    condition rendaring sign in and out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
 import { useState } from 'react';
 const auth=getAuth(app)
@@ -18,17 +18,34 @@ function App() {
       console.log.error('error',error);
     })
   }
+  const handleSignout=()=>{
+
+
+
+const auth = getAuth();
+signOut(auth)
+.then(() => {
+  setUser({});
+})
+.catch((error) => {
+  setUser({});
+});
+
+  }
   return (
     <div className="App">
-      <button onClick={HandleGoogleSignin} >Google Sign in</button>
-      <div>
-        user Name: {user.displayName}
-      </div>
+      {
+        user.email ?  <button onClick={handleSignout} >Sign Out</button>:
+        <button onClick={HandleGoogleSignin} >Google Sign in</button> 
+      }
 
-      <div>
-        Email Address: {user.email}
-      </div>
-      <img src={user.photoURL} alt=""/>
+     {
+      user.email &&  <div>
+      <h3> user Name: {user.displayName}</h3>
+     <p> Email Address: {user.email}</p>
+     <img src={user.photoURL} alt=""/>
+     </div>
+     }
       
     </div>
   );
